4b0a0586621e5bcd072b0d766330821da0e4cc2d
Phat <pattrickha@gmail.com>
2021-10-20 12:46:59 +0700

    Add project files.

---
+++ SolarSystem.Data/Entities/Body.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SolarSystem.Data.Entities
+{
+    public class Body
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double EarthMass { get; set; }
+        public string DistanceToTheSun { get; set; }
+    }
+}
