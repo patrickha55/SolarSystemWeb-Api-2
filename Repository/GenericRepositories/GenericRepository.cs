013b5d45d020d50b5c7829d02e99b6a2d33a8490
Phat <pattrickha@gmail.com>
2021-10-21 14:42:15 +0700

    Add Generic Repository and Autofac

---
+++ Repository/GenericRepositories/GenericRepository.cs
@@ -0,0 +1,47 @@
+﻿using Data;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.GenericRepositories
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private readonly ApplicationContext _context;
+        private readonly DbSet<T> _db;
+
+        public GenericRepository(ApplicationContext context)
+        {
+            _context = context;
+            _db = _context.Set<T>();
+        }
+
+        public Task<IList<T>> GetAll(System.Linq.Expressions.Expression<Func<T, bool>> expression, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, List<string> includes = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> Get(System.Linq.Expressions.Expression<Func<T, bool>> expression, List<string> includes = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Create(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
