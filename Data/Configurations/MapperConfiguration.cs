9893d47c5e3d4154903ec063c2fb011d31bc4e89
Phat <pattrickha@gmail.com>
2021-10-21 18:57:58 +0700

    Added Auto Mapper nuget package and changed DTO classes

---
+++ Data/Configurations/MapperConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Data.DTOs;
+using Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Configurations
+{
+    public class MapperConfiguration : Profile
+    {
+        public MapperConfiguration()
+        {
+            CreateMap<Region, RegionDTO>().ReverseMap();
+        }
+    }
+}
